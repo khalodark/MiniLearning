1d6c78b0d033ca2d348479ae9a30815c442ca707
Khalid <khalidqadamani@gmail.com>
2025-03-06 03:06:56 +0300

    adjust authRepository accordingly

--- shared/src/commonMain/kotlin/org/khalodark/miniudemy/repository/AuthRepository.kt
+++ shared/src/commonMain/kotlin/org/khalodark/miniudemy/repository/AuthRepository.kt
@@ -1,31 +1,37 @@
 package org.khalodark.miniudemy.repository
 
-import io.ktor.client.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import kotlinx.serialization.Serializable
+import io.ktor.client.HttpClient
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.serialization.json.Json
 import org.khalodark.miniudemy.SERVER_PORT
-
-@Serializable
-data class LoginRequest(val email: String, val password: String)
-
-@Serializable
-data class LoginResponse(val token: String, val userId: String)
+import org.khalodark.miniudemy.data.LoginRequest
+import org.khalodark.miniudemy.data.LoginResponse
 
 /**
  * Simple class for requesting login to local host using post
  */
 class AuthRepository(private val client: HttpClient) {
 
-    suspend fun login(email: String, password: String): LoginResponse {
-        val response: HttpResponse = client.post("http://localhost:${SERVER_PORT}/login") {
-            contentType(ContentType.Application.Json)
-            setBody(LoginRequest(email, password))
+    fun login(email: String, password: String): Flow<Result<LoginResponse>> = flow {
+        try {
+            val response: HttpResponse = client.post("http://localhost:${SERVER_PORT}/login") {
+                contentType(ContentType.Application.Json)
+                setBody(LoginRequest(email, password))
+            }
+            val json = Json { ignoreUnknownKeys = true } // prevent problems
+            val loginResponse = json.decodeFromString<LoginResponse>(response.bodyAsText())
+            emit(Result.success(loginResponse))
+        } catch (e: Exception) {
+            emit(Result.failure(e))
         }
-        val json = Json { ignoreUnknownKeys = true } // prevent problems
-        return json.decodeFromString<LoginResponse>(response.bodyAsText())
+
     }
 
 }
